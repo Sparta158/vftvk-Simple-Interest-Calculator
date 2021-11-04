1c1e4d57681190bfd2fbf0aa39f7a75d44409f01
Sparta158 <88295253+Sparta158@users.noreply.github.com>
2021-11-04 18:53:33 -0400

    erw

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function compute()
 {
-    var p = document.getElementById('principal').value;
-    var r = document.getElementById('rate').value;
-    var t = document.getElementById('years').value;
-    var totalinterest = p * r * t;
-    document.getElementById('text1').value=p;
-    document.getElementById('text2').value=r+"%";
-    document.getElementById('text3').value=totalinterest;
-
+  //  var p = document.getElementById('principal').value;
+   // var r = document.getElementById('rate').value;
+   // var t = document.getElementById('years').value;
+    //var totalinterest = p * r * t;
+   // document.getElementById('text1').value=p;
+   // document.getElementById('text2').value=r+"%";
+   // document.getElementById('text3').value=totalinterest;
+console.log('shit')
 
 }
 function updateTextInput(val)
