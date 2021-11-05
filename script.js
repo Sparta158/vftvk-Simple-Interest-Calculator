e8eb6c26747f5f7f5676e0a1aaf8e3c8d7f7792b
Sparta158 <88295253+Sparta158@users.noreply.github.com>
2021-11-04 23:43:47 -0400

    Real Close minus pretty

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-function compute()
+function compute(p,r,t)
 {
-  //  var p = document.getElementById('principal').value;
-   // var r = document.getElementById('rate').value;
-   // var t = document.getElementById('years').value;
-    //var totalinterest = p * r * t;
-   // document.getElementById('text1').value=p;
-   // document.getElementById('text2').value=r+"%";
-   // document.getElementById('text3').value=totalinterest;
-   document.getElementById("texthere").innerHTML = "Whoo";
+    var p = document.getElementById('principal').value;
+    var r = document.getElementById('rate').value;
+    var t = document.getElementById('years').value;
+    var totalinterest = p * r / 100 * t;
+    var ptext = p.toString();
+    var rtext = r.toString();
+    var answer = totalinterest.toString();
+    var thisyear = new Date().getFullYear();
+    var year = parseInt(thisyear);
+    year += t;
+    document.getElementById("text1").innerHTML="If you deposit "+ ptext+",";
+    document.getElementById("text2").innerHTML="at an interest rate of "+ rtext+"%.";
+    document.getElementById("text3").innerHTML="You will receive an amount of "+ answer+",";
+    document.getElementById("text4").innerHTML="in the year "+ year;
+
+   document.getElementById("texthere").innerHTML = val;
 
 }
 function updateTextInput(val)
 {
-    document.getElementById('textInput').value=val+"%";
+   document.getElementById("texthere").innerHTML=val+"%";
     
     //var text=document.createTextNode('TextyTextyText');
     //var target = document.getElementById('texthere');
