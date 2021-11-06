cca3aa0e63cc4f95d13b1124e8d9be307505ab92
Sparta158 <88295253+Sparta158@users.noreply.github.com>
2021-11-06 18:19:12 -0400

    Final 211106
    Jeez

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ function compute(p,r,t)
     var p = document.getElementById('principal').value;
     var r = document.getElementById('rate').value;
     var t = document.getElementById('years').value;
+    if (p<=0) {
+        document.getElementById("alert").innerHTML="Please enter a positive number";
+        document.getElementById("text1").innerHTML="";
+        document.getElementById("text2").innerHTML="";
+        document.getElementById("text3").innerHTML="";
+        document.getElementById("text4").innerHTML="";
+    } else{
+    document.getElementById("alert").innerHTML="";
     var totalinterest = p * r / 100 * t;
     var ptext = p.toString();
     var rtext = r.toString();
     var answer = totalinterest.toString();
-    var duration = 12*t;
     var year = new Date();
     var yearint = year.getFullYear();
     var monthint = year.getMonth();
@@ -16,8 +23,13 @@ function compute(p,r,t)
     document.getElementById("text2").innerHTML="at an interest rate of "+ rtext+"%.";
     document.getElementById("text3").innerHTML="You will receive an amount of "+ answer+",";
     document.getElementById("text4").innerHTML="in the year "+ future.getFullYear();
+    }
 }
 function updateTextInput(val)
 {
    document.getElementById("texthere").innerHTML=val+"%";
+}
+function ShiftFocus()
+{
+    document.getElementById("principal").focus();
 }
