44a612067a7832783737522a8567313a7a1d9c05
Sparta158 <88295253+Sparta158@users.noreply.github.com>
2021-11-05 20:54:08 -0400

    Finalish

--- script.js
+++ script.js
@@ -7,22 +7,17 @@ function compute(p,r,t)
     var ptext = p.toString();
     var rtext = r.toString();
     var answer = totalinterest.toString();
-    var thisyear = new Date().getFullYear();
-    var year = parseInt(thisyear);
-    year += t;
+    var duration = 12*t;
+    var year = new Date();
+    var yearint = year.getFullYear();
+    var monthint = year.getMonth();
+    var future = new Date(yearint, 12*t+monthint);
     document.getElementById("text1").innerHTML="If you deposit "+ ptext+",";
     document.getElementById("text2").innerHTML="at an interest rate of "+ rtext+"%.";
     document.getElementById("text3").innerHTML="You will receive an amount of "+ answer+",";
-    document.getElementById("text4").innerHTML="in the year "+ year;
-
-   document.getElementById("texthere").innerHTML = val;
-
+    document.getElementById("text4").innerHTML="in the year "+ future.getFullYear();
 }
 function updateTextInput(val)
 {
    document.getElementById("texthere").innerHTML=val+"%";
-    
-    //var text=document.createTextNode('TextyTextyText');
-    //var target = document.getElementById('texthere');
-   // target.parentNode.insertBefore(text, target);
 }
